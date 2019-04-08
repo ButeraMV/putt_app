0ab54814be37a1fd66136ea684d6c27dca04d086
Boots2291 <buteramv@gmail.com>
2019-04-08 09:34:16 -0400

    Add list of contest spec

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -1,4 +1,7 @@
 class ContestsController < ApplicationController
   def index
   end
+
+  def new
+  end
 end
--- spec/features/homepage/contest_index_spec.rb
+++ spec/features/homepage/contest_index_spec.rb
@@ -1,8 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Contest Index', :type => :feature do
-  scenario 'navigating to root path' do
+  before(:each) do
     visit '/'
+  end
+
+  scenario 'navigating to root path' do
     expect(page).to have_content('Putting Contest')
+  end
+
+  scenario 'link to new contest path' do
+    click_link 'New Contest'
+    expect(current_path).to eq(new_contest_path)
+  end
+
+  scenario 'a list of contests is displayed' do
+    contest = create(:contest)
+    expect(page).to have_content(contest.name)
   end
 end
